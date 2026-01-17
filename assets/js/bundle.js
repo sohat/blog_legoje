3fe41d7162b64445da4c5959bd350724be808d8e
sohapark <sohot@kbs.co.kr>
2026-01-17 23:33:04 +0900

    bundle.js 공백제거

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,5 +1,8 @@
 ---
 ---
+{% capture jscode %}
 {% include_relative menu.js %}
 {% include_relative toc.js %}
+{% endcapture %}
+{{ jscode | strip_newlines | replace: '  ', '' | replace: '	', '' }}
 ;
